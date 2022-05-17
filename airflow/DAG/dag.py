2f472de451dfcfabc1f49ed6432bc3feae24e6ae
KonKinPak <tiewclub9@gmail.com>
2022-05-17 23:43:26 +0700

    dag

--- airflow/DAG/dag.py
+++ airflow/DAG/dag.py
@@ -1,3 +1,4 @@
+from airflow.scraping.scrapeData import scrapeAllStation
 from airflow.utils.dates import days_ago
 from airflow import DAG
 
@@ -7,10 +8,12 @@ from airflow.operators.python import PythonOperator
 
 with DAG(
     'scrape',
-    default_args=default_args,
     description='Scrape data every hour',
     tags=['pm2.5_dashboard'],
     schedule_interval='@hourly',
     catchup = False
 ) as dag:
-    scrape = PythonOperator(task_id='inform_status', python_callable=show_status, op_args=[ping.output, ping2.output])
+    scrape = PythonOperator(task_id='scrapeAllStations', python_callable=scrapeAllStations)
+
+
+    scrape
