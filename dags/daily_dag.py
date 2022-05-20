4578ac0ad9d0364087e91ce2b28a189c7066fe4f
KonKinPak <tiewclub9@gmail.com>
2022-05-20 20:18:04 +0700

    revised train

--- dags/daily_dag.py
+++ dags/daily_dag.py
@@ -1,21 +1,20 @@
-from airflow.utils.dates import days_ago
 from airflow import DAG
 
 from airflow.operators.bash import BashOperator
 from airflow.operators.dummy import DummyOperator
 from airflow.operators.python import PythonOperator
-from airflow.providers.http.operators.http import SimpleHttpOperator
 
 from datetime import datetime, timedelta, timezone
 from service.senderBIAPI import sendToBI
+from service.train import inputModel
 
 #start date use utc+0
 
-with DAG('hourly_dag',default_args={'retries': 5,'retry_delay': timedelta(seconds=30)}, description='Train and predict daily',tags=['pm2.5_dashboard'],schedule_interval='@daily'
+with DAG('daily_dag',default_args={'retries': 5,'retry_delay': timedelta(seconds=30)}, description='Train and predict daily',tags=['pm2.5_dashboard'],schedule_interval='@daily'
 ,start_date= datetime(2022, 5, 19,17),catchup = True,max_active_runs=1) as dag:
 
-    feedModel = PythonOperator(task_id='feedModel', python_callable=feedModel)
-    delayDummy = BashOperator(task_id="dummy_delay_1",bash_command="sleep 30s")
+    feedModel = PythonOperator(task_id='feedModel', python_callable=inputModel)
+    delayDummy = BashOperator(task_id="dummy_delay_1",bash_command="sleep 60s")
     sendPredictedPM = PythonOperator(task_id='sendPredictedPM', python_callable=sendToBI,op_args=['predicted'])
     success = DummyOperator(task_id="success")
 
--- dags/service/senderBIAPI.py
+++ dags/service/senderBIAPI.py
@@ -1,3 +1,4 @@
+import time
 import requests
 import pandas as pd
 import psycopg2
@@ -64,13 +65,103 @@ def sendToBI(typeS):
     "Content-Type": "application/json"
     }
 
-  def push_streaming_data(data):
+  def push_streaming_data(data,count):
+    print('------------------ Trying -----------------------')
+    count+=1
+    print('trials : ', count)
+    if(count >= 10):
+      return
     response = requests.request(
         method="POST",
         url=url,
         headers=headers,
         data=json.dumps(data)
     )
+    if response.status_code != 200:
+      print(response.status_code,response.text)
+      # time.sleep(30)
+      push_streaming_data(data,count)
+    print('------------------ Success -----------------------')         
     return response
 
-  print(push_streaming_data(parsed))
+  print(push_streaming_data(parsed,0))
+
+
+
+def sendToBI2(typeS):
+  print('connecting to DB . . .')
+  conn = psycopg2.connect(database='airflow', user='airflow', password='airflow',host='172.24.0.2')
+  cursor = conn.cursor()
+  print('connected to DB !')
+
+  if typeS == 'history':
+    queryStr = 'SELECT * FROM raw_data ORDER BY id DESC LIMIT 341;'
+    columns = ['id', 'device', 'lat', 'lng', 'pm25', 'pm10', 'rh', 'temp', 'datetime_aq']
+
+  elif typeS == 'predicted':
+    #**********************************************************
+    queryStr = 'SELECT * FROM predicted_data ORDER BY id DESC LIMIT 24552;'
+    columns = ['id', 'device', 'lat', 'lng', 'value', 'date']
+
+  cursor.execute(queryStr)
+  conn.commit()
+
+  results = cursor.fetchall()
+
+  conn.close()
+
+  print('------------------------------ done Query ! -----------------------------------')
+
+
+  df = pd.DataFrame(results,columns=columns)
+  df['type'] = typeS
+
+  if typeS == 'history':
+    df.drop([ "pm10", "rh", "temp"], axis='columns',inplace=True)
+
+    name_mapper = {
+        'pm25': 'value',
+        'datetime_aq': 'date'
+    }
+    df.rename(columns=name_mapper,inplace=True)
+
+  # ------------------- start adding device name -------------------
+  df_with_device_name = pd.read_csv('/opt/airflow/dags/service/ID_INFO_SFA.csv')
+  df_with_device_name = df_with_device_name.rename({'device_id': 'device'}, axis='columns')
+
+  # left join and change "name_en" ---> "device"
+  df = pd.merge(df,df_with_device_name[['device', 'name_en']],on='device',how='left')
+  df = df.drop('device', axis='columns')
+  df = df.rename({'name_en': 'device'}, axis='columns')
+
+
+  result = df.to_json(orient="records")
+  parsed = json.loads(result)
+  json.dumps(parsed, indent=4)
+
+  url = "https://api.powerbi.com/beta/271d5e7b-1350-4b96-ab84-52dbda4cf40c/datasets/13bc3203-b5d8-4f4a-acc9-f87f570adc0b/rows?key=L9hFXv6c5G3VfLjFF0dFSJs6TtySEYD3uZFoSp%2F12QmLMahbYJs7U60lTQyMJDeHJt0gtyw64vvLpuqjouMfag%3D%3D"
+
+  headers = {
+    "Content-Type": "application/json"
+    }
+
+  def push_streaming_data(data,count):
+    print('------------------ Trying -----------------------')
+    count+=1
+    print('trials : ', count)
+    if(count >= 10):
+      return
+    response = requests.request(
+        method="POST",
+        url=url,
+        headers=headers,
+        data=json.dumps(data)
+    )
+    if response.status_code != 200:
+      print(response.status_code,response.text)
+      # time.sleep(30)
+      push_streaming_data(data,count)
+    print('------------------ Success -----------------------')         
+    return response
+
+  print(push_streaming_data(parsed,0))
