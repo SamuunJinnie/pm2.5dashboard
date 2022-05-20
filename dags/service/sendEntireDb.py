733b094a1b6e9bf41247b49c661076a43c7c8196
puiza <saran.swpy@gmail.com>
2022-05-20 17:23:24 +0700

    add POST entire DB to jinnie

---
+++ dags/service/sendEntireDb.py
@@ -0,0 +1,63 @@
+import requests
+import pandas as pd
+import psycopg2
+import random
+import json
+
+from datetime import datetime
+
+conn = psycopg2.connect(
+    database='airflow', 
+    user='airflow', 
+    password='airflow',
+    host='172.24.0.2'
+)
+cursor = conn.cursor()
+
+query = "SELECT * FROM raw_data;"
+cursor.execute(query)
+conn.commit()
+results = cursor.fetchall()
+
+columns = ['id', 'device', 'lat', 'lng', 'pm25', 'pm10', 'rh', 'temp', 'datetime_aq']
+df = pd.DataFrame(results, columns=columns)
+
+df = df.drop(['pm10', 'rh', 'temp'], axis='columns')
+name_mapper = {
+    'pm25': 'value',
+    'datetime_aq': 'date'
+}
+df.rename(columns=name_mapper,inplace=True)
+
+# ------------------- start adding device name -------------------
+df_with_device_name = pd.read_csv('/opt/airflow/dags/service/ID_INFO_SFA.csv')
+df_with_device_name = df_with_device_name.rename({'device_id': 'device'}, axis='columns')
+
+# left join and change "name_en" ---> "device"
+df = pd.merge(df,df_with_device_name[['device', 'name_en']],on='device',how='left')
+df = df.drop('device', axis='columns')
+df = df.rename({'name_en': 'device'}, axis='columns')
+
+
+
+result = df.to_json(orient="records")
+parsed = json.loads(result)
+json.dumps(parsed, indent=4)
+
+url = "https://api.powerbi.com/beta/271d5e7b-1350-4b96-ab84-52dbda4cf40c/datasets/7822ddc3-6b79-4dd6-bc8b-d020325c743c/rows?key=T87n6eQ%2FwSuzoWoRhYzHrzDCkyUrxtBh1rWRj567bbfRaggrlD7cGjrpJN49NfTdXG62RN91x%2FtcaEzy7Reb6Q%3D%3D"
+
+headers = {   
+    "Content-Type": "application/json"
+}
+
+def push_streaming_data(data):
+    response = requests.request(
+        method="POST",
+        url=url,
+        headers=headers,
+        data=json.dumps(data)
+    )
+    return response
+
+
+print(push_streaming_data(parsed))
